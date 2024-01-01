1b5099f659b880fa6005044eff30950f5ebeb6e5
killgriff22 <kilgrif22033@gmail.com>
2024-01-01 17:49:38 -0500

    test :3

--- classes.py
+++ classes.py
@@ -1,9 +1,24 @@
 from modules import *
-class Screen:
-    def __init__(self,size:tuple,pos:tuple) -> None:
+class canvas:
+    def __init__(self,size:tuple[int,int]) -> None:
         self.size = size
+        self.content = [[" "]*size[0] for _ in range(size[1])]
+    def fill(self,char:str) -> None:
+        for x in range(0,self.size[0]):
+            for y in range(0,self.size[1]):
+                self.content[y][x] = char
+        return
+    def blit(self,text:str,pos:tuple[int,int]):
+        n=self.size[0]
+        lines=[list(text[i:i+n]) for i in range(0, len(text), n)]
+        for y,line in enumerate(lines):
+            for x,char in enumerate(line):
+                self.content[pos[1]+y][pos[0]+x] = char
+
+class Screen(canvas):
+    def __init__(self,size:tuple,pos:tuple) -> None:
+        super().__init__(size)
         self.pos = pos
-        self.content=[[" "]*self.size[0] for _ in range(self.size[1])]
     def draw(self) -> None:
         for i,line in enumerate(self.content):
             if len(line)-1 > self.size[0]:
@@ -20,17 +35,3 @@ class Screen:
             lines.append(" "*self.size[0])
         for i,line in enumerate(lines):
             print_at(self.pos[0]+1,self.pos[1]+i+1,line)
-    def fill(self,char:str) -> None:
-        for x in range(0,self.size[0]):
-            for y in range(0,self.size[1]):
-                self.content[y][x] = char
-        return
-class Process:
-    def __init__(self,screen:Screen,path:str or os.PathLike, args:list[str]=[]):
-        self.screen = screen
-        self.output=str()
-        self.Process = subprocess.Popen([path]+args,stdout=self.output)
-    def draw(self):
-        for x,char in self.output:
-            y=x//self.screen.size[0]
-            self.screen.content[y][x] = char
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from classes import *
 w,h=os.get_terminal_size()
-display = Screen((w,h//2),(0,0))
-display2 = Screen((w,h//2),(0,h//2))
+display = Screen((w,h),(0,0))
+#display2 = Screen((w,h//2),(0,h//2))
 display.fill(info(" "))
-display2.fill(warn(" "))
+#display2.fill(warn(" "))
+display.blit("test",(0,0))
 while True:
     try:
         display.draw()
-        display2.draw()
+#        display2.draw()
     except KeyboardInterrupt:
         clear()
         exit()
