2d166b7cce8da71a9702d022eadb7def0ca4c9b5
killgriff22 <kilgrif22033@gmail.com>
2024-01-01 18:07:52 -0500

    that was bad, this should be fine

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from classes import *
+SafeZone = eval(compile(open("config.py").read(),"config","eval"))
+w,h=os.get_terminal_size()
+display = Screen((w-SafeZone[0]*2,h-SafeZone[1]*2),SafeZone)
+#display2 = Screen((w,h//2),(0,h//2))
+display.fill(info(" "))
+#display2.fill(warn(" "))
+display.blit("test",(0,0))
+clear()
 while True:
-    SafeZone = eval(compile(open("config.py").read(),"config","eval"))
-    w,h=os.get_terminal_size()
-    display = Screen((w-SafeZone[0]*2,h-SafeZone[1]*2),SafeZone)
-    #display2 = Screen((w,h//2),(0,h//2))
-    display.fill(info(" "))
-    #display2.fill(warn(" "))
-    display.blit("test",(0,0))
     try:
         display.draw()
-        clear()
 #        display2.draw()
     except KeyboardInterrupt:
         clear()
