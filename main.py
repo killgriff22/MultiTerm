9623426e174e69cb287c94b359ab165d796c933a
killgriff22 <kilgrif22033@gmail.com>
2024-01-01 17:25:44 -0500

    more :)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from classes import *
 w,h=os.get_terminal_size()
-display = Screen((w,h),(0,0))
+display = Screen((w,h//2),(0,0))
+display2 = Screen((w,h//2),(0,h//2))
 display.fill(info(" "))
+display2.fill(warn(" "))
 while True:
     try:
         display.draw()
+        display2.draw()
     except KeyboardInterrupt:
         clear()
         exit()
