b039fb45cbfb5836758bb8c6786a52bb9c70ed75
killgriff22 <kilgrif22033@gmail.com>
2024-01-01 18:20:15 -0500

    YIPPIE

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from classes import *
 displays = cluster()
 SafeZone = eval(compile(open("config.py").read(),"config","eval"))
-w,h=os.get_terminal_size()
-display = Screen((w-SafeZone[0]*2,h-SafeZone[1]*2),SafeZone)
-#display2 = Screen((w,h//2),(0,h//2))
-display.fill(info(" "))
-#display2.fill(warn(" "))
-display.blit("test",(0,0))
-displays.screens.append(display)
+exec(compile(open("screens.py").read(),"screens","exec"))
 clear()
 while True:
     try:
---
+++ screens.py
@@ -0,0 +1,7 @@
+w,h=os.get_terminal_size()
+display = Screen((w-SafeZone[0]*2,h-SafeZone[1]*2),SafeZone)
+#display2 = Screen((w,h//2),(0,h//2))
+display.fill(info(" "))
+#display2.fill(warn(" "))
+display.blit("test",(0,0))
+displays.screens.append(display)
