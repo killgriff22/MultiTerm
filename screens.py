609f3f882efd3c5e95497e9eb2c649c98f274d68
killgriff22 <kilgrif22033@gmail.com>
2024-01-01 19:13:13 -0500

    fuckx2

--- screens.py
+++ screens.py
@@ -1,12 +1,11 @@
 w,h=os.get_terminal_size()
 display = Screen((w-SafeZone[0]*2,h-SafeZone[1]*2),SafeZone)
 #display2 = Screen((w,h//2),(0,h//2))
-display.fill(info(" "))
 #display2.fill(warn(" "))
-display.blit("test",(0,0))
 displays.screens.append(display)
 import datetime
 while True:
     time=datetime.datetime.now().strftime("%H:%M:%S %d/%m/%Y")
+    display.fill(info(" "))
     display.blit(time,(display.size[0]//2-len(time),display.size[1]//2))
     displays.draw_all()
