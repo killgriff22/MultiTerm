22891ef78463fe5a70f9c6bfbb0e138f090e85ab
killgriff22 <kilgrif22033@gmail.com>
2024-01-05 11:09:29 -0500

    Testing new canvas.blit functionality

--- screens.py
+++ screens.py
@@ -1,4 +1,5 @@
 import datetime
+from classes import *
 w, h = os.get_terminal_size()
 display = Screen((w-SafeZone[0]*2, h-SafeZone[1]*2), SafeZone)
 # display2 = Screen((w,h//2),(0,h//2))
@@ -6,6 +7,7 @@ display = Screen((w-SafeZone[0]*2, h-SafeZone[1]*2), SafeZone)
 displays.screens.append(display)
 while True:
     time = datetime.datetime.now().strftime("%H:%M:%S %d/%m/%Y")
-    display.fill(info(" "))
-    display.blit(time, (display.size[0]//2-len(time), display.size[1]//2))
+    display.fill(error(" "))
+    display.blit(time+"\nTest\nTest2Test3\nTest4", (display.size[0]//2-len(time), display.size[1]//2),
+                 front_modifier=Fore.BLACK+Back.RED, back_modifier=RESET)
     displays.draw_all()
