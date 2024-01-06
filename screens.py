e6497f512599be868f30778ff015def5682b09bd
killgriff22 <kilgrif22033@gmail.com>
2024-01-05 23:45:06 -0500

    test3

--- screens.py
+++ screens.py
@@ -14,8 +14,8 @@ while True:
     time = datetime.datetime.now()
     Hours = f.renderText(time.strftime("%H/%M/%S"))
     Date = f.renderText(time.strftime("%d/%m/%Y"))
-    display.blit(Hours, (0, 0), front_modifier=Fore.BLACK +
+    display.blit(Hours, (w//2-len(Hours.split("\n")[0]), h//2-len(Hours.split("\n"))//2), front_modifier=Fore.BLACK +
                  Back.RED, back_modifier=RESET)
-    display.blit(Date, (0, len(Hours.split("\n"))),
+    display.blit(Date, (w//2-len(Hours.split("\n")[0]), h//2+len(Hours.split("\n"))),
                  front_modifier=Fore.BLACK+Back.RED, back_modifier=RESET)
     displays.draw_all()
