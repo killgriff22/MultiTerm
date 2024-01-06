e81255e0b8360b02b05126b73f94535a3148174a
killgriff22 <kilgrif22033@gmail.com>
2024-01-05 23:49:11 -0500

    heheh

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ clear()
 process = subprocess.Popen(['python3', 'screens.py'])
 oldcontent = hash(open("screens.py", "r").read())
 while True:
+    subprocess.check_output(
+        ["git", "pull"], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
     if hash(open("screens.py", "r").read()) == oldcontent:
         pass
     else:
