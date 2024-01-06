cfc3ae74c8c2e94c37dddb5e77d5cc0429842079
killgriff22 <kilgrif22033@gmail.com>
2024-01-05 23:56:15 -0500

    ok this for sure because i forgot about commiting this

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from modules import *
 clear()
 process = subprocess.Popen(['python3', 'screens.py'])
 oldcontent = hash(open("screens.py", "r").read())
-while True:
-    subprocess.check_output(
-        ["git", "pull"])
-    if hash(open("screens.py", "r").read()) == oldcontent:
-        time.sleep(1)
-    else:
-        process.kill()
-        process = subprocess.Popen(['python3', 'screens.py'])
-        oldcontent = hash(open("screens.py", "r").read())
+try:
+    while True:
+        subprocess.check_output(
+            ["git", "pull"])
+        if hash(open("screens.py", "r").read()) == oldcontent:
+            time.sleep(1)
+        else:
+            process.kill()
+            process = subprocess.Popen(['python3', 'screens.py'])
+            oldcontent = hash(open("screens.py", "r").read())
+except KeyboardInterrupt:
+    process.kill()
+    print("Exiting...")
+    exit()
