ab0529c80f75c45d9fe8a501f8a4f781db84e5bc
killgriff22 <kilgrif22033@gmail.com>
2024-01-06 23:36:21 -0500

    EHOH

--- screens.py
+++ screens.py
@@ -7,5 +7,6 @@ display = Screen((w-SafeZone[0]*2, h-SafeZone[1]*2), SafeZone)
 while True:
     display.fill(error(" "))
     for i in range(1000):
-        display.content[0][1] = Back.WHITE+" "+RESET
+        display.content[random.randint(
+            0, h)][1] = Back.WHITE+" "+RESET
     display.draw()
