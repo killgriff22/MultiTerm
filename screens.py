e5a4a0453c4e6a1dac442ee70edd4f4b4aa5829b
killgriff22 <kilgrif22033@gmail.com>
2024-01-06 20:25:12 -0500

    bit denser

--- screens.py
+++ screens.py
@@ -6,7 +6,7 @@ SafeZone = (3, 1)
 display = Screen((w-SafeZone[0]*2, h-SafeZone[1]*2), SafeZone)
 while True:
     display.fill(" ")
-    for i in range(100):
+    for i in range(1000):
         display.blit(" ", (random.randint(0, w), random.randint(0, h)),
                      front_modifier=Back.WHITE, back_modifier=RESET)
     display.draw()
