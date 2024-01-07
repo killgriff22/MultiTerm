4371085b160ced7b35fba33c4d6aa272ce2e7a30
killgriff22 <kilgrif22033@gmail.com>
2024-01-06 23:39:13 -0500

    test

--- screens.py
+++ screens.py
@@ -1,13 +1,15 @@
 from classes import *
 import random
+from playsound import playsound
 clear()
 w, h = os.get_terminal_size()
 SafeZone = (3, 1)
 display = Screen((w-SafeZone[0]*2, h-SafeZone[1]*2), SafeZone)
+playsound("Future - Mask Off.mp3")
 while True:
     display.fill(error(" "))
-    for i in range(1000):
+    """for i in range(1000):
         display.content[random.randint(
             0, display.size[1])][random.randint(
-                0, display.size[0])] = Back.WHITE+" "+RESET
+                0, display.size[0])] = Back.WHITE+" "+RESET"""
     display.draw()
