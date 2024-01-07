631e174e78383a5ef03fb6e220dff81a7786a0bd
killgriff22 <kilgrif22033@gmail.com>
2024-01-06 20:22:58 -0500

    Blank Screen

--- screens.py
+++ screens.py
@@ -4,5 +4,5 @@ w, h = os.get_terminal_size()
 SafeZone = (3, 1)
 display = Screen((w-SafeZone[0]*2, h-SafeZone[1]*2), SafeZone)
 while True:
-    display.fill(error(" "))
+    display.fill(" ")
     display.draw()
