28afcb861ef63843390250a17c351b4b613b2615
killgriff22 <kilgrif22033@gmail.com>
2024-01-06 23:41:50 -0500

    maybee??

--- screens.py
+++ screens.py
@@ -1,11 +1,15 @@
 from classes import *
 import random
-from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
+
+# for playing mp3 file
 clear()
 w, h = os.get_terminal_size()
 SafeZone = (3, 1)
 display = Screen((w-SafeZone[0]*2, h-SafeZone[1]*2), SafeZone)
-playsound("Future - Mask Off.mp3")
+song = AudioSegment.from_mp3("Future - Mask Off.mp3")
+play(song)
 while True:
     display.fill(error(" "))
     """for i in range(1000):
